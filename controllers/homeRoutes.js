a81399d8a506c5e67c77c24c4cf36f4792bad617
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-11-07 15:11:19 -0800

    Add get routes for all posts and  all comments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,19 +6,62 @@ router.get('/', async (req, res) => {
   try {
     const postData = await Post.findAll({
       attributes: { exclude: ['id', 'content', 'date_created', 'user_id'] },
-      order: [['date_created' 'DESC']],
+      order: [['date_created', 'DESC']],
     });
 
     const posts = postData.map((post) => post.get({ plain: true }));
 
     res.status(200).render('homepage', { posts });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
+// route to get all posts from one username
+router.get('/posts/:username', async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { username: req.params.username },
+      attributes: ['id', 'username'],
+    });
+
+    if (!userData) {
+      res.status(404).json({ success: false, message: 'No user found with this username!' });
+      return;
+    }
+
+    const postData = await Post.findAll({
+      where: { user_id: userData.id },
+      attributes: { exclude: ['user_id'] },
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+          as: 'author',
+        },
+        {
+          model: Comment,
+          attributes: ['text'],
+          include: [
+            {
+              model: User,
+              attributes: ['username'],
+            },
+          ],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.status(200).render('posts', { posts });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
   }
 });
 
 
-// route to get one post
+// route to get one post by ID
 router.get('/post/:id', async (req, res) => {
   try{ 
       const postData = await Post.findByPk(req.params.id, {
@@ -43,19 +86,60 @@ router.get('/post/:id', async (req, res) => {
       });
 
       if(!postData) {
-          res.status(404).json({message: 'No post with this id!'});
+          res.status(404).json({ success: false, message: 'No post with this id!' });
           return;
       }
 
       const post = postData.get({ plain: true });
 
-      res.status(200).render('post', post);
+      res.status(200).render('post', { post });
     } catch (err) {
-        res.status(500).json(err);
+        res.status(500).json({ success: false, error: err.message });
     };     
 });
 
-// route to get one comment
+// route to get all comments from one username
+router.get('/comments/:username', async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { username: req.params.username },
+      attributes: ['id', 'username'],
+    });
+
+    if (!userData) {
+      res.status(404).json({ success: false, message: 'No user found with this username!' });
+      return;
+    }
+
+    const commentsData = await Comment.findAll({
+      where: { user_id: userData.id },
+      attributes: { exclude: ['id', 'user_id', 'post_id'] },
+      include: [
+        { model: User, attributes: ['username'] },
+        {
+          model: Post,
+          attributes: ['id', 'title'],
+          include: [
+            {
+              model: User,
+              attributes: ['username'],
+              as: 'author',
+            },
+          ],
+        },
+      ],
+    });
+
+    const comments = commentsData.map((comment) => comment.get({ plain: true }));
+
+    res.status(200).render('comments', { comments });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
+
+// route to get one comment by ID
 router.get('/comment/:id', async (req, res) => {
   try{ 
       const commentData = await Comment.findByPk(req.params.id, {
@@ -85,7 +169,7 @@ router.get('/comment/:id', async (req, res) => {
 
       res.status(200).render('comment', comment);
     } catch (err) {
-        res.status(500).json(err);
+      res.status(500).json({ success: false, error: err.message });
     };     
 });
 
