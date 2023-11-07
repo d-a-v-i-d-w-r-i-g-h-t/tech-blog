6924153b1f5c6cf690b3e3ea1637051861d08dcd
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-11-06 19:22:05 -0800

    Add seed data, seeds.js

---
+++ models/index.js
@@ -0,0 +1,37 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// set up associations
+
+// user & post: one to many
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+// user & comment: one to many
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+// post & comment: one to many
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+module.exports = { User, Post, Comment };
