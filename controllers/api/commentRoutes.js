33b536035e665e38c7fab942a2d5de1d9d9ddfd1
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-11-06 23:42:16 -0800

    Add post and comment routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,3 +1,64 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/authorize');
+
+// POST route to create a comment
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(201).json({ success: true, data: newComment });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message });
+  }
+});
+
+// PUT route to update a comment
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.update(
+      {
+        text: req.body.text,
+      },
+      {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      }
+    );
+
+    if (!commentData[0]) {
+      res.status(404).json({ success: false, message: 'No comment found with this id!' });
+      return;
+    }
+
+    res.status(200).json({ success: true, message: 'Comment updated successfully!' });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
+// DELETE route to delete a comment
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!commentData) {
+      res.status(404).json({ success: false, message: 'No comment found with this id!' });
+      return;
+    }
+
+    res.status(200).json({ success: true, message: 'Comment deleted successfully!' });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,3 +1,65 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/authorize');
+
+// POST route to create a post
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(201).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message });
+  }
+});
+
+// PUT route to update a post
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      }
+    );
+
+    if (!postData[0]) {
+      res.status(404).json({ success: false, message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json({ success: true, message: 'Post updated successfully!' });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
+// DELETE route to delete a post
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ success: false, message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json({ success: true, message: 'Post deleted successfully!' });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
