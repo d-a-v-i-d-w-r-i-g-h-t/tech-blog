0c70e5cd7191c650a26aa841a953a0db41d77a78
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-11-16 15:46:14 -0800

    Add bootstrap

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers'); // DELETE IF NOT NECESSARY
@@ -7,6 +8,8 @@ const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
 
 // Create the Handlebars.js engine object with custom helper functions
 const hbs = exphbs.create({ helpers: helpers }); // DELETE IF NOT NECESSARY
