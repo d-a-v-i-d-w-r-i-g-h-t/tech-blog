ce1814c2bb2a82125ba874f1b4339ff20050a639
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-09 13:09:45 -0800

    Change cookie max age to 2 min for demo purposes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const sess = {
   secret: process.env.SESSION_SECRET || "default-secret-key",
   cookie: {
-    maxAge: 86391000,
+    maxAge: 120000, // two minutes
     httpOnly: true,
     secure: false,
     saveSite: "strict",
